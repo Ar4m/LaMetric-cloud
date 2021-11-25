b47f01cab75f89ace96373c3c2c213b1c9dc9d33
Ar4m <a-j.tosbath@orange.fr>
2021-11-25 15:41:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
+app.listen(process.env.PORT,() => {
+    console.log("Server started");
+})
+
 app.get('/gettime', gettime);
 
 function gettime(req,res){
-    //let timezone = req.query.
-    request("https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Paris", function*(Body){
+    let timezone = req.query.timezone || "Europe/Paris";
+    request("https://www.timeapi.io/api/Time/current/zone?timeZone="+timezone, function*(Body){
         let jsonobject = JSON.parse(Body);
 
         let content = {
